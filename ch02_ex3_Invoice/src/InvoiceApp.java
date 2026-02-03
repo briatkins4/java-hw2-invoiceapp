9beed206d7a41c40a9f7d72bcd875813fc4e259c
Nadie0816 <64112529+Nadie0816@users.noreply.github.com>
2026-02-02 23:00:19 -0500

    modify code for invoices

--- ch02_ex3_Invoice/src/InvoiceApp.java
+++ ch02_ex3_Invoice/src/InvoiceApp.java
@@ -12,6 +12,11 @@ public class InvoiceApp {
 
         // perform invoice calculations until choice isn't equal to "y" or "Y"
         String choice = "y";
+
+        int invoiceCount = 0;
+        double invoiceTotal = 0;
+        double discountTotal = 0;
+
         while (!choice.equalsIgnoreCase("n")) {
             // get the invoice subtotal from the user
             System.out.print("Enter subtotal:   ");
@@ -35,15 +40,29 @@ public class InvoiceApp {
             double total = subtotal - discountAmount;
 
             // display the discount amount and total
-            String message = "Number of invoices: " + discountPercent + "\n"
-                           + "Average invoice:  " + discountAmount + "\n"
-                           + "Average discount:    " + total + "\n";            
+            String message = "Discount percent: " + discountPercent + "\n"
+                           + "Discount amount:  " + discountAmount + "\n"
+                           + "Invoice total:    " + total + "\n";            
             System.out.println(message);
+
+            invoiceCount = invoiceCount + 1;
+            invoiceTotal = invoiceTotal + total;
+            discountTotal = discountTotal + discountAmount;
 
             // see if the user wants to continue
             System.out.print("Continue? (y/n): ");
             choice = sc.nextLine();
             System.out.println();
         }
+        double averageInvoice = invoiceTotal / invoiceCount;
+        double averageDiscount = discountTotal / invoiceCount;
+
+            // display the discount amount and total
+            String summary = "Number of invoices: " + invoiceCount + "\n"
+                           + "Average invoice:  " + averageInvoice + "\n"
+                           + "Average discount:    " + averageDiscount + "\n";            
+            System.out.println(summary);
+
+            sc.close();
     }
 }
